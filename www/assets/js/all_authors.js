16cb4a67c44682327e1a750959157ff6101b45f3
Blanca <blanluja@gmail.com>
2019-06-07 17:36:48 +0200

    holiwinki

---
+++ www/assets/js/all_authors.js
@@ -0,0 +1,66 @@
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+  
+    if ("withCredentials" in xhr) {
+  
+      // Check if the XMLHttpRequest object has a "withCredentials" property.
+      // "withCredentials" only exists on XMLHTTPRequest2 objects.
+      xhr.open(method, url, true);
+  
+    } else if (typeof XDomainRequest != "undefined") {
+  
+      // Otherwise, check if XDomainRequest.
+      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+  
+    } else {
+  
+      // Otherwise, CORS is not supported by the browser.
+      xhr = null;
+  
+    }
+    
+    return xhr;
+  
+}
+
+$(document).ready(function(){
+    
+
+    // $("button").click(function(){
+         
+         console.log("Button clicked");
+ 
+         var url = '/v2/authors';
+ 
+         var xhttp = createCORSRequest('GET', url);
+         if (!xhttp) {
+             throw new Error('CORS not supported');
+         }
+ 
+         xhttp.onload = function() {
+           
+           var text = xhttp.responseText;
+           //$("#response").text(text);
+ 
+           var myJSON = text;
+           var myObj = JSON.parse(myJSON);
+           $("#query").text(myObj.length);
+ 
+           for(var i =0; i<myObj.length;i++){
+             var name =myObj[i].name;
+             var bio = myObj[i].bio;
+             var picture = myObj[i].picture;
+             var id = myObj[i].id;
+             newListElement(name, bio, picture, id, "AllAuthors");
+           }
+         };
+ 
+         xhttp.send();
+ 
+   //  });
+ 
+ });
+ 
+ 
--- www/assets/js/favourites.js
+++ www/assets/js/favourites.js
@@ -1,6 +1,51 @@
 
+function printListGenre(name){
+  console.log(name);
+
+  var element = document.createElement("div");
+  element.className = "p-2 flex-fill";
+  var genre = document.createElement("a");
+  var textGenre = document.createTextNode(name);
+
+
+  genre.href = "genres.html?name="+ name;
+
+  genre.appendChild(textGenre);
+  element.appendChild(genre);
+  document.getElementById("flex_genres").appendChild(element);
+}
+
+function printGenres(){
+  var url = '/v2/genres/getGenres';
+
+
+  var xhttp = createCORSRequest('GET', url);
+  if (!xhttp) {
+      throw new Error('CORS not supported');
+  }
+
+  xhttp.onload = function() {
+      
+    var text = xhttp.responseText;
+
+    var myJSON = text;
+    var myObj = JSON.parse(myJSON);
+
+  //tener en cuenta que puede ser más de un autor(a)
+    for(var i =0; i<myObj.length;i++){
+      var genre = myObj[i].name;
+      //$("#textito").text(theme);
+      printListGenre(genre);
+    }
+  }
+
+  xhttp.send();
+}
+
+
 $(document).ready(function(){
      
+    printGenres();
     printElementsGroup("favourites");
   
   });
